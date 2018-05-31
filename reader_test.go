60ca6d4f057e93b8d12757cf38e393cdbd92c504
Julien Fabre <ju.pryz@gmail.com>
2018-05-31 10:22:17 -0700

    increase timeout

--- reader_test.go
+++ reader_test.go
@@ -788,7 +788,7 @@ func TestReaderConsumerGroup(t *testing.T) {
 			})
 			defer r.Close()
 
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 			defer cancel()
 
 			test.function(t, ctx, r)
